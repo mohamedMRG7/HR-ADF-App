6ac5fab256aa331bb2a2c2810e155e2547c6c88d
Mohamed Ragab <devmohamed1325@gmail.com>
2019-04-12 22:54:52 +0200

    search and delete department finished

---
+++ ViewController/public_html/departments/DepartmentBindingIDS.java
@@ -0,0 +1,15 @@
+package departments;
+
+public class DepartmentBindingIDS {
+    /*CUSTOM VIEW BINDING ATTRIBUTE*/
+    public static final String DEPARTMENT_ID="DepartmentId1";
+    public static final String DEPARTMENT_NAME="DepartmentName1";
+    public static final String MANAGER_ID="ManagerId1";
+    public static final String LOCATION_ID="LocationId1";
+    
+    /*ITERATORS*/
+    public static final String CUSTOMSEARCHVO_ITERATOR_ID="DepartmentsCustomSearchROVIterator";
+    public static final String DEPARTMENTVO_ITERATOR="DepartmentsVOIterator";
+
+
+}
--- ViewController/src/view/MainClass.java
+++ ViewController/src/view/MainClass.java
@@ -1,23 +1,44 @@
 package view;
 
+import departments.DepartmentBindingIDS;
+
 import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
+import oracle.adf.model.BindingContext;
+import oracle.adf.model.binding.DCBindingContainer;
+import oracle.adf.model.binding.DCIteratorBinding;
+import oracle.adf.view.rich.component.rich.RichPopup;
 import oracle.adf.view.rich.component.rich.layout.RichPanelFormLayout;
 import oracle.adf.view.rich.component.rich.layout.RichPanelGroupLayout;
 import oracle.adf.view.rich.component.rich.layout.RichPanelSplitter;
 import oracle.adf.view.rich.component.rich.nav.RichLink;
 import oracle.adf.view.rich.context.AdfFacesContext;
 
+import oracle.adfdt.model.objects.IteratorBinding;
+
+import oracle.binding.AttributeBinding;
+import oracle.binding.BindingContainer;
+
+import oracle.jbo.ViewObject;
+
 import org.apache.myfaces.trinidad.event.AttributeChangeEvent;
 
 public class MainClass {
-    private boolean readOnlyView=true;
-    private boolean edaitableView=false;
+    private boolean readOnlyView = true;
+    private boolean edaitableView = false;
     private RichPanelFormLayout readOnlyForm;
     private RichPanelGroupLayout editableForm;
+    private boolean searchViewEnabled = false;
+    private RichPanelGroupLayout searchViewForm;
+    private RichPopup deletePopUpView;
 
     public MainClass() {
+        //get the value from the session to maintain the search view after click search or reset
+        if (getSessionVariable("searchViewEnabled") != null) {
+            searchViewEnabled = (Boolean) getSessionVariable("searchViewEnabled");
+        }
     }
 
     public void setReadOnlyView(boolean readOnlyView) {
@@ -36,18 +57,6 @@ public class MainClass {
         return edaitableView;
     }
 
-    public void edite(ActionEvent actionEvent) {
-        
-        //Replace readOnly form with editable form when click on edite button 
-        setReadOnlyView(false);
-        setEdaitableView(true);
-        //Refresh the both form
-        addPartialTrigger(getEditableForm());
-        addPartialTrigger(getReadOnlyForm());
-      
-       
-    }
-
     public void setReadOnlyForm(RichPanelFormLayout readOnlyForm) {
         this.readOnlyForm = readOnlyForm;
     }
@@ -63,14 +72,54 @@ public class MainClass {
     public RichPanelGroupLayout getEditableForm() {
         return editableForm;
     }
-    
-    //add partial trigger to component 
-    public void addPartialTrigger(UIComponent ui)
-    {
-         AdfFacesContext.getCurrentInstance().addPartialTarget(ui);
+
+    public void setSearchViewForm(RichPanelGroupLayout searchViewForm) {
+        this.searchViewForm = searchViewForm;
     }
 
-   
+    public RichPanelGroupLayout getSearchViewForm() {
+        return searchViewForm;
+    }
+
+    public void setSearchViewEnabled(boolean searchViewEnabled) {
+        this.searchViewEnabled = searchViewEnabled;
+    }
+
+    public boolean getSearchViewEnabled() {
+        return searchViewEnabled;
+    }
+
+    public void setDeletePopUpView(RichPopup deletePopUpView) {
+        this.deletePopUpView = deletePopUpView;
+    }
+
+    public RichPopup getDeletePopUpView() {
+        return deletePopUpView;
+    }
+
+
+    /*
+     *
+     *
+     *
+     *Action lisners And Actions
+     *
+     *
+     *
+     * */
+
+
+    public void edite(ActionEvent actionEvent) {
+
+        //Replace readOnly form with editable form when click on edite button
+        setReadOnlyView(false);
+        setEdaitableView(true);
+        //Refresh the both form
+        addPartialTrigger(getEditableForm());
+        addPartialTrigger(getReadOnlyForm());
+
+
+    }
 
     public String finishEdite() {
         // Add event code here...
@@ -81,4 +130,121 @@ public class MainClass {
         addPartialTrigger(getReadOnlyForm());
         return null;
     }
+
+
+    public void openSearchView(ActionEvent actionEvent) {
+
+        setSearchViewEnabled(true);
+        //To maintain the seach view open after click search or reset
+        setSessionVariable("searchViewEnabled", true);
+        addPartialTrigger(getSearchViewForm());
+    }
+
+    public void cancelSearch(ActionEvent actionEvent) {
+        // Add event code here...
+        setSearchViewEnabled(false);
+        setSessionVariable("searchViewEnabled", false);
+        addPartialTrigger(getSearchViewForm());
+    }
+
+    public void resetDepartmentSearch(ActionEvent actionEvent) {
+        // Add event code here...
+        setAttributeBindingInputeValue(DepartmentBindingIDS.DEPARTMENT_ID,null);
+        setAttributeBindingInputeValue(DepartmentBindingIDS.DEPARTMENT_NAME,null);
+        setAttributeBindingInputeValue(DepartmentBindingIDS.MANAGER_ID,null);
+        setAttributeBindingInputeValue(DepartmentBindingIDS.LOCATION_ID,null);
+    }
+
+    public void departmentSearch(ActionEvent actionEvent) {
+        // Add event code here...
+
+
+        String departmentID = (String) getAttributeBindingInputeValue(DepartmentBindingIDS.DEPARTMENT_ID);
+        String departmentName = (String) getAttributeBindingInputeValue(DepartmentBindingIDS.DEPARTMENT_NAME);
+        String mangerId = (String) getAttributeBindingInputeValue(DepartmentBindingIDS.MANAGER_ID);
+        String locationID = (String) getAttributeBindingInputeValue(DepartmentBindingIDS.LOCATION_ID);
+
+        DCBindingContainer dcContainer = (DCBindingContainer) BindingContext.getCurrent().getCurrentBindingsEntry();
+        DCIteratorBinding departmentiterator =
+            (DCIteratorBinding) dcContainer.get(DepartmentBindingIDS.DEPARTMENTVO_ITERATOR);
+        ViewObject departmentVO = departmentiterator.getViewObject();
+        departmentVO.setNamedWhereClauseParam("P_DEPT_ID", departmentID);
+        departmentVO.setNamedWhereClauseParam("P_DEPT_NAME", departmentName);
+        departmentVO.setNamedWhereClauseParam("MANGER_ID", mangerId);
+        departmentVO.setNamedWhereClauseParam("P_LOCATION_ID", locationID);
+
+        departmentVO.executeQuery();
+
+    }
+
+
+    public void showdeletePopUp(ActionEvent actionEvent) {
+        showPopUp(getDeletePopUpView());
+    }
+
+
+    public void delete(ActionEvent actionEvent) {
+        // Add event code here...
+        excuteBindingOperation("Delete");
+        excuteBindingOperation("Commit");
+        hidePopUp(getDeletePopUpView());
+    }
+
+    public void cancelPopUp(ActionEvent actionEvent) {
+        // Add event code here...
+        hidePopUp(getDeletePopUpView());
+    }
+
+    /*
+     *
+     *
+     * Custom Methods
+     *
+     */
+
+
+    //add partial trigger to component
+    public void addPartialTrigger(UIComponent ui) {
+        AdfFacesContext.getCurrentInstance().addPartialTarget(ui);
+    }
+
+    public static void setSessionVariable(String key, Object value) {
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put(key, value);
+    }
+
+    public static Object getSessionVariable(String key) {
+        return FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(key);
+    }
+
+    public static Object getAttributeBindingInputeValue(String bindingAtrrID) {
+        BindingContext bContext = BindingContext.getCurrent();
+        BindingContainer bContainer = bContext.getCurrentBindingsEntry();
+        AttributeBinding attr = (AttributeBinding) bContainer.getControlBinding(bindingAtrrID);
+        return attr.getInputValue();
+    }
+    
+    public void setAttributeBindingInputeValue(String attrID,Object value)
+    {
+        BindingContext bContext = BindingContext.getCurrent();
+        BindingContainer bContainer = bContext.getCurrentBindingsEntry();
+        AttributeBinding attr = (AttributeBinding) bContainer.getControlBinding(attrID);
+        attr.setInputValue(value);
+    }
+
+    public void excuteBindingOperation(String operationID) {
+        BindingContext bContext = BindingContext.getCurrent();
+        BindingContainer bContainer = bContext.getCurrentBindingsEntry();
+        bContainer.getOperationBinding(operationID).execute();
+    }
+
+    public void showPopUp(RichPopup popUpCom) {
+        RichPopup.PopupHints hints = new RichPopup.PopupHints();
+        popUpCom.show(hints);
+    }
+
+    public void hidePopUp(RichPopup popUpCom) {
+        popUpCom.hide();
+    }
+
+
 }
